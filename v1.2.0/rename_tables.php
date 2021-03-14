912164b7e63a0b867889173a11cf6a2b9f3600cc
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-14 18:09:05 -0400

    add db tables rename migration file

---
+++ v1.2.0/rename_tables.php
@@ -0,0 +1,34 @@
+<?php namespace Winter\Location\Updates;
+
+use Schema;
+use Winter\Storm\Database\Updates\Migration;
+
+class RenameTables extends Migration
+{
+    const TABLES = [
+        'countries',
+        'states',
+    ];
+
+    public function up()
+    {
+        foreach (self::TABLES as $table) {
+            $from = 'rainlab_location_' . $table;
+            $to   = 'winter_location_' . $table;
+            if (Schema::hasTable($from)) {
+                Schema::rename($from, $to);
+            }
+        }
+    }
+
+    public function down()
+    {
+        foreach (self::TABLES as $table) {
+            $from = 'winter_location_' . $table;
+            $to   = 'rainlab_location_' . $table;
+            if (Schema::hasTable($from)) {
+                Schema::rename($from, $to);
+            }
+        }
+    }
+}
