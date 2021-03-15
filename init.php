0e10a9b414bb8309720666b60c56a99e4d1327be
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-15 09:58:42 -0400

    prevent creating the class aliases when the original plugin is still installed

--- init.php
+++ init.php
@@ -1,13 +1,15 @@
 <?php
 
-class_alias(Winter\Location\Plugin::class, Winter\Location\Plugin::class);
+if (!class_exists(RainLab\Location\Plugin::class)) {
+    class_alias(Winter\Location\Plugin::class, RainLab\Location\Plugin::class);
 
-class_alias(Winter\Location\Behaviors\definition::class, Winter\Location\Behaviors\definition::class);
+    class_alias(Winter\Location\Behaviors\definition::class, RainLab\Location\Behaviors\definition::class);
 
-class_alias(Winter\Location\Controllers\Locations::class, Winter\Location\Controllers\Locations::class);
+    class_alias(Winter\Location\Controllers\Locations::class, RainLab\Location\Controllers\Locations::class);
 
-class_alias(Winter\Location\FormWidgets\AddressFinder::class, Winter\Location\FormWidgets\AddressFinder::class);
+    class_alias(Winter\Location\FormWidgets\AddressFinder::class, RainLab\Location\FormWidgets\AddressFinder::class);
 
-class_alias(Winter\Location\Models\State::class, Winter\Location\Models\State::class);
-class_alias(Winter\Location\Models\Setting::class, Winter\Location\Models\Setting::class);
-class_alias(Winter\Location\Models\Country::class, Winter\Location\Models\Country::class);
+    class_alias(Winter\Location\Models\State::class, RainLab\Location\Models\State::class);
+    class_alias(Winter\Location\Models\Setting::class, RainLab\Location\Models\Setting::class);
+    class_alias(Winter\Location\Models\Country::class, RainLab\Location\Models\Country::class);
+}
