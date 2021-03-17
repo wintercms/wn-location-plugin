29e2ec65fff1db37b4e8b1d5dd4a197dec3ca6a6
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-17 12:07:29 -0400

    normalize class names

--- aliases.php
+++ aliases.php
@@ -8,12 +8,12 @@ use Winter\Storm\Support\ClassLoader;
  */
 $aliases = [
     // Regular aliases
-    Winter\Location\Plugin::class                     => 'RainLab\Location\Plugin',
-    Winter\Location\Controllers\Locations::class      => 'RainLab\Location\Controllers\Locations',
-    Winter\Location\FormWidgets\AddressFinder::class  => 'RainLab\Location\FormWidgets\AddressFinder',
-    Winter\Location\Models\State::class               => 'RainLab\Location\Models\State',
-    Winter\Location\Models\Setting::class             => 'RainLab\Location\Models\Setting',
-    Winter\Location\Models\Country::class             => 'RainLab\Location\Models\Country',
+    Winter\Location\Plugin::class                     => RainLab\Location\Plugin::class,
+    Winter\Location\Controllers\Locations::class      => RainLab\Location\Controllers\Locations::class,
+    Winter\Location\FormWidgets\AddressFinder::class  => RainLab\Location\FormWidgets\AddressFinder::class,
+    Winter\Location\Models\State::class               => RainLab\Location\Models\State::class,
+    Winter\Location\Models\Setting::class             => RainLab\Location\Models\Setting::class,
+    Winter\Location\Models\Country::class             => RainLab\Location\Models\Country::class,
 ];
 
 app(ClassLoader::class)->addAliases($aliases);
