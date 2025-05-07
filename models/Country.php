a0725b13e213bb6903df652194af8d05e9abef24
Marc Jauvin <marc.jauvin@gmail.com>
2025-05-07 10:08:21 -0400

    remove return type in signature as it can be none

--- models/Country.php
+++ models/Country.php
@@ -118,7 +118,7 @@ class Country extends Model
      *
      * @return array
      */
-    public function getIsoAttribute(): ?array
+    public function getIsoAttribute()
     {
         if (self::$iso) {
             return self::$iso;
